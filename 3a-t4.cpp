ede250cafa31efb70be5e8ac47fd47bd061b3ac1
kamisarenkaa <93708920+kamisarenkaa@users.noreply.github.com>
2021-12-03 12:11:48 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,44 +1,22 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
-int main()	
+int main()
 {
-//declaring variables
-int price = 0;
-int number = 5; 
-int nds = 20;
-/* declaring and initializing pointers */
-int *ptx = &price; 
-int *pty = &number; 
-int *ptz = &nds;
-// variables values output
-cout << "Starting values:" << endl;
-cout << "price = " << price << endl; 
-cout << "number = " << number << endl; 
-cout << "nds = " << nds << endl;
-// Changing values directly 
-price = price + 30; 
-number = number + 100; 
-nds = nds + 3;
-//values after the direct assignments
-cout << "variables values after direct change:" << endl;
-cout << "price = " << price << endl; 
-cout << "number = " << number << endl; 
-cout << "nds = " << nds << endl;
-// changing values by using pointers
-*ptx = *ptx + 10; 
-*pty = *pty + 20;
-*ptz = *ptz + 2; 
-/* values after the indirect assignments */
-cout << " variables values after indirect change:" << endl;
-cout << "price = " << price << endl; 
-cout << "number = " << number << endl; 
-cout << "nds = " << nds << endl;
-cout << endl;
-//indirect access to values
- 
-cout << "Accessing values indirectly:" << endl;
-cout<<"price (*ptx) = " << *ptx<<endl; 
-cout<<"number (*pty) = "<< *pty<<endl; 
-cout<<"nds (*ptz) = "<< *ptz << endl;
-return 0; 
+int x, y = 1, sum_x = 0, n; 
+cout << " Enter the amount of integers to sum = ";
+cin >> x;
+if(x==0){
+cout << " The total sum of 0 integers is: 0";
+return 0;
 }
+do {
+cout << "Enter integer nr. " << y << ": ";
+cin >> n;
+sum_x += n;
+y++;
+}
+while (y <= x);
+cout << " The total sum of " << x << " integers is: " << sum_x;
+return 0;
+}
+
