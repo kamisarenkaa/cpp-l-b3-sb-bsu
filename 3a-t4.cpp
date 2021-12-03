e77f8cc143c33e1e3dcd1ed8212ca4c8feab5732
kamisarenkaa <93708920+kamisarenkaa@users.noreply.github.com>
2021-12-03 12:15:30 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -2,21 +2,30 @@
 using namespace std;
 int main()
 {
-int x, y = 1, sum_x = 0, n; 
+int x, y = 1, sum_x = 0, number;
 cout << " Enter the amount of integers to sum = ";
 cin >> x;
-if(x==0){
-cout << " The total sum of 0 integers is: 0";
-return 0;
+while (!cin || (cin.peek() != '\n'))
+{
+cin.clear();
+while (cin.get() != '\n');
+cout << "Please, enter an integer" << endl;
+cin >> x;
 }
+if(x==0){cout << " The total sum of 0 integers is: 0"; return 0;}
 do {
 cout << "Enter integer nr. " << y << ": ";
-cin >> n;
-sum_x += n;
-y++;
+cin >> number;
+while (!cin || (cin.peek() != '\n'))
+{
+cin.clear();
+while (cin.get() != '\n');
+cout << "Please, enter an integer" << endl;
+cin >> number;
 }
-while (y <= x);
+sum_x += number;
+y++;
+} while (y <= x);
 cout << " The total sum of " << x << " integers is: " << sum_x;
 return 0;
 }
-
